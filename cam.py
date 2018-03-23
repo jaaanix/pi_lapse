76fd171b2870f4387f991beedcc19fc3dad3f5d2
jaaanix <janis.saritzoglou@opitz-consulting.de>
2018-03-23 14:08:23 +0000

    implements the first version of pi cam timelapse, able to be started with one parameter for the time range shots are made.

---
+++ cam.py
@@ -0,0 +1,23 @@
+from time import sleep
+from picamera import PiCamera
+import subprocess
+import sys
+
+print 'Start PiCam timelapse image capturing...'
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument list:', str(sys.argv)
+print 'First argument: Capture interval in seconds=', str(int(sys.argv[1]))
+
+camera = PiCamera()
+camera.resolution = (1280, 720)
+camera.start_preview()
+
+sleep(2)
+
+for x in range(10):
+    print 'Capturing picture %s' % (x)
+    camera.capture('%s_img.jpg' % (x))
+    sleep(int(sys.argv[1]))
+
+subprocess.call(['ffmpeg', '-y',  '-r', '5', '-start_number', '0', '-i', '%d_img.jpg', '-s', '1280x720', '-vcodec', 'libx264', 'timelapse.mp4'])
+
