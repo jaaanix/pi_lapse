2febb0b2181997051abe273a206c1925bfe4537a
jaaanix <janis.saritzoglou@opitz-consulting.de>
2018-04-17 22:17:03 +0200

    adds more arguments to control the amount of pictures taken and the fps value for rendering.

--- cam.py
+++ cam.py
@@ -6,7 +6,9 @@ import sys
 print 'Start PiCam timelapse image capturing...'
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument list:', str(sys.argv)
-print 'First argument: Capture interval in seconds=', str(int(sys.argv[1]))
+print 'First argument: Capture interval in seconds =', str(int(sys.argv[1]))
+print 'Second argument: Amount of picture taken =', str(int(sys.argv[2]))
+print 'Third argument: FPS for rendering =', str(int(sys.argv[3]))
 
 camera = PiCamera()
 camera.resolution = (1280, 720)
@@ -14,10 +16,10 @@ camera.start_preview()
 
 sleep(2)
 
-for x in range(10):
+for x in range(int(sys.argv[2])):
     print 'Capturing picture %s' % (x)
     camera.capture('%s_img.jpg' % (x))
     sleep(int(sys.argv[1]))
 
-subprocess.call(['ffmpeg', '-y',  '-r', '5', '-start_number', '0', '-i', '%d_img.jpg', '-s', '1280x720', '-vcodec', 'libx264', 'timelapse.mp4'])
+subprocess.call(['ffmpeg', '-y',  '-r', '20', '-start_number', '0', '-i', '%d_img.jpg', '-s', '1280x720', '-vcodec', 'libx264', 'timelapse.mp4'])
 
